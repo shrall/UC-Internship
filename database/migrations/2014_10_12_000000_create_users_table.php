1a675c25175763f4796c08e799c79dab1fb20f14
shrall <shrallvierdo@gmail.com>
2020-12-14 16:38:18 +0700

    update polymorphism in users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,8 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('is_login', ['0','1'])->default('0');
+            $table->integer('detailable_id')->unsigned();
+            $table->string('detailable_type');
             $table->rememberToken();
             $table->timestamps();
         });
