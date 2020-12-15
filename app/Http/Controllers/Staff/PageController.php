3a0d04087f5d09c6cb75e19e2295b66c79203ff1
shrall <shrallvierdo@gmail.com>
2020-12-15 16:08:21 +0700

    fix controller template

--- app/Http/Controllers/Staff/PageController.php
+++ app/Http/Controllers/Staff/PageController.php
@@ -7,8 +7,5 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    public function dashboard(){
-        $pages = 'dash';
-        return view('admin.dashboard', compact('pages'));
-    }
+    //
 }
