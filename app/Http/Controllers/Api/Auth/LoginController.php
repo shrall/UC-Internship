e8b03efc7abf117e0ac9443affba2a7b027ae74d
shrall <shrallvierdo@gmail.com>
2020-12-16 19:56:48 +0700

    api login, logout, refresh for student done.

---
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Laravel\Passport\Client;
+use GuzzleHttp\Client as GuzzleHttpClient;
+
+class LoginController extends Controller
+{
+    private $client;
+
+    public function __construct()
+    {
+        $this->client = Client::find(2);
+    }
+
+    public function login(Request $request)
+    {
+
+        $http = new GuzzleHttpClient;
+
+        $student = [
+            'email' => $request->email,
+            'password' => $request->password,
+            'role_id' => 3,
+            'is_login' => '0',
+        ];
+
+        $check = DB::table('uci_users')->where('email', $request->email)->first();
+
+        if ($check->is_login == '0') {
+            if (Auth::attempt($student)) {
+                $this->is_login(Auth::id());
+                $response = $http->post('http://uci.test/oauth/token', [
+                    'form_params' => [
+                        'grant_type' => 'password',
+                        'client_id' => $this->client->id,
+                        'client_secret' => $this->client->secret,
+                        'username' => $request->email,
+                        'password' => $request->password,
+                        'scope' => '*',
+                    ]
+                ]);
+                return json_decode((string) $response->getBody(), true);
+            } else {
+                return response([
+                    'message' => 'Login Failed'
+                ]);
+            }
+        } else {
+            return response([
+                'message' => 'Account is already logged in.'
+            ]);
+        }
+    }
+
+    private function is_login(int $id)
+    {
+        $user = User::findOrFail($id);
+        return $user->update([
+            'is_login' => '1',
+        ]);
+    }
+
+    public function refresh(Request $request)
+    {
+        $this->validate($request, [
+            'refresh_token' => 'required',
+        ], [
+            'refresh_token' => 'refresh token is required'
+        ]);
+
+        $http = new GuzzleHttpClient;
+
+        $response = $http->post('http://uci.test/oauth/token', [
+            'form_params' => [
+                'grant_type' => 'refresh_token',
+                'client_id' => $this->client->id,
+                'client_secret' => $this->client->secret,
+                'refresh_token' => $request->refresh_token,
+                'scope' => '*',
+            ]
+        ]);
+        return json_decode((string) $response->getBody(), true);
+    }
+
+    public function logout()
+    {
+        $user = Auth::user();
+        $accesstoken = Auth::user()->token();
+        DB::table('oauth_refresh_tokens')->where('access_token_id', $accesstoken->id)->update(['revoked' => true]);
+
+        $user->update([
+            'is_login' => '0',
+        ]);
+
+        $accesstoken->revoke();
+
+        return response([
+            'message' => 'Logged Out.'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('api-login', [LoginController::class, 'login']);
+Route::post('api-refresh', [LoginController::class, 'refresh']);
+
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::post('api-logout', [LoginController::class, 'logout']);
 });
