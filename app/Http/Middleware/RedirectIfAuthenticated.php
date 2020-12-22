9d62fef2026011878de1a92a7c0132ca9d35f0d7
shrall <shrallvierdo@gmail.com>
2020-12-22 15:18:01 +0700

    redirect to dashboard if logged in

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -23,7 +23,13 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                if (Auth::user()->isStudent()) {
+                    return redirect()->route('student.dashboard');
+                } else if (Auth::user()->isSupervisor()) {
+                    return redirect()->route('supervisor.dashboard');
+                } else if (Auth::user()->isAdmin()) {
+                    return redirect()->route('admin.dashboard');
+                }
             }
         }
 
