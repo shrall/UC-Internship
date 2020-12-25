b555a4e9d557d52d9319659ee8f26dfd41f9d593
shrall <shrallvierdo@gmail.com>
2020-12-25 20:56:42 +0700

    remove $info from admin's controllers

--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -13,12 +13,7 @@ class PageController extends Controller
     public function dashboard()
     {
         $pages = 'dash';
-        if (Auth::user()->detailable_type == "App\Models\Staff") {
-            $info = Staff::find(Auth::user()->detailable_id);
-        } else if (Auth::user()->detailable_type == "App\Models\Lecturer") {
-            $info = Lecturer::find(Auth::user()->detailable_id);
-        }
         $projects = Project::all();
-        return view('admin.dashboard', compact('pages', 'info', 'projects'));
+        return view('admin.dashboard', compact('pages', 'projects'));
     }
 }
