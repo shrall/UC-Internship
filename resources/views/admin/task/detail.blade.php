ea5d6d5b7283fffb01d303834ee170ebd0b9318c
shrall <shrallvierdo@gmail.com>
2020-12-26 23:44:28 +0700

    admin task page done

---
+++ resources/views/admin/task/detail.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+@section('content')
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
+    <div class="d-block mb-4 mb-md-0">
+        <nav aria-label="breadcrumb" class="d-none d-md-inline-block">
+            <ol class="breadcrumb breadcrumb-dark breadcrumb-transparent">
+                <li class="breadcrumb-item">
+                    <a href="{{ route('admin.dashboard') }}">
+                        <span class="fas fa-home"></span>
+                    </a>
+                </li>
+                <li class="breadcrumb-item">Projects</li>
+                <li class="breadcrumb-item">
+                    <a href="{{ route('admin.project.index') }}">Project List</a>
+                </li>
+                <li class="breadcrumb-item" aria-current="page">
+                    <a href="{{ route('admin.project.show', $task->projectuser->uci_project_id) }}">{{ $task->projectuser->uci_project_id }}</a>
+                </li>
+                <li class="breadcrumb-item active" aria-current="page">
+                    <a href="{{ route('admin.task.show', $task->id) }}">{{ $task->name }}</a>
+                </li>
+            </ol>
+        </nav>
+    </div>
+</div>
+<div class="row mt-2">
+    @include('admin.task.card.detail')
+    @include('admin.task.card.pic')
+</div>
+@include('admin.task.table.progress')
+@endsection
---
+++ resources/views/admin/task/table/progress.blade.php
@@ -0,0 +1,60 @@
+<div class="row">
+    <div class="col-12 mb-4">
+        <div class="card border-light shadow-sm">
+            <div class="card-header border-0 pb-2">
+                <div class="row align-items-center">
+                    <div class="col">
+                        <h2 class="h4"><i class="fas fa-spinner mr-2"></i>Progress List</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="card card-body border-light shadow-sm table-wrapper table-responsive">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th class="border-0">Description</th>
+                            <th class="border-0" width="150px">Time Start</th>
+                            <th class="border-0" width="150px">Time End</th>
+                            <th class="border-0" width="150px">Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($task->progresses as $progress)
+                        <!-- Item -->
+                        @include('admin.task.modal.progress')
+                        <tr>
+                            <td>
+                                <a data-toggle="modal" data-target="#modal-progress-{{$progress->id}}">{{$progress->description}}</a>
+                            </td>
+                            <td>
+                                {{ date('d-m-Y, H:i', strtotime($progress->time_start)) }}
+                            </td>
+                            <td>
+                                {{ date('d-m-Y, H:i', strtotime($progress->time_end)) }}
+                            </td>
+                            @if ($progress->status == 0)
+                            <td class="text-warning">
+                                <span class="fas fa-clock"></span>
+                                <span class="font-weight-bold">Pending</span>
+                            </td>
+                            @endif
+                            @if ($progress->status == 1)
+                            <td class="text-success">
+                                <span class="fas fa-check"></span>
+                                <span class="font-weight-bold">Approved</span>
+                            </td>
+                            @endif
+                            @if ($progress->status == 2)
+                            <td class="text-danger">
+                                <span class="fas fa-times"></span>
+                                <span class="font-weight-bold">Declined</span>
+                            </td>
+                            @endif
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
