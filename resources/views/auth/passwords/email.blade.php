58bdb4d2fdbd2c716c5992a2f2854e3685fb8fe5
shrall <shrallvierdo@gmail.com>
2020-12-27 22:06:04 +0700

    fix routing to login from lost password view

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -4,8 +4,9 @@
 <section class="vh-lg-100 mt-lg-0 bg-soft d-flex align-items-center">
     <div class="container">
         <div class="row justify-content-center form-bg-image">
-            <p class="text-center"><a href="./sign-in.html" class="text-gray-700"><i
-                        class="fas fa-angle-left mr-2"></i>Back to log in</a></p>
+            <p class="text-center">
+                <a href="{{ route('login') }}" class="text-gray-700">
+                    <i class="fas fa-angle-left mr-2"></i>Back to log in</a></p>
             <div class="col-12 d-flex align-items-center justify-content-center">
                 <div
                     class="signin-inner my-3 my-lg-0 bg-white shadow-soft border rounded border-light p-4 p-lg-5 w-100 fmxw-500">
@@ -29,9 +30,9 @@
                         <div class="mb-4">
                             <label for="email">Your Email</label>
                             <div class="input-group">
-                                <input id="email" type="email" class="form-control"
-                                    name="email" value="{{ old('email') }}" required
-                                    autocomplete="email" autofocus>
+                                <input id="email" type="email" class="form-control" name="email"
+                                    value="{{ old('email') }}" required autocomplete="email"
+                                    autofocus>
                             </div>
                         </div>
                         <button type="submit" class="btn btn-block btn-primary">Recover password</button>
