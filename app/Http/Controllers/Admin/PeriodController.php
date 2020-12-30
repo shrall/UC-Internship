93e735a18515c19263c62b2b4eb0d9441659ecf8
shrall <shrallvierdo@gmail.com>
2020-12-30 19:36:38 +0700

    admin - period - done

--- app/Http/Controllers/Admin/PeriodController.php
+++ app/Http/Controllers/Admin/PeriodController.php
@@ -15,7 +15,9 @@ class PeriodController extends Controller
      */
     public function index()
     {
-        //
+        $pages = 'period';
+        $periods = Period::all();
+        return view('admin.period.index', compact('pages', 'periods'));
     }
 
     /**
@@ -25,7 +27,8 @@ class PeriodController extends Controller
      */
     public function create()
     {
-        //
+        $pages = 'period';
+        return view('admin.period.create', compact('pages'));
     }
 
     /**
@@ -36,7 +39,35 @@ class PeriodController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->validate([
+            'term' => 'required',
+            'start' => 'required|date',
+            'end' => 'required|date',
+        ]);
+
+        if ($data['start'] >= $data['end']) {
+            return redirect()->route('admin.period.create')->with('Error', 'The starting date cannot exceed the end date');
+        }
+        $periods = Period::all();
+
+        foreach ($periods as $period) {
+            if ($data['start'] >= $period->start && $data['start'] <= $period->end) {
+                return redirect()->route('admin.period.create')->with('Error', 'There is already an ongoing period from ' . date('jS, F Y', strtotime($period->start)) . ' to ' . date('jS, F Y', strtotime($period->end)));
+            }
+            if ($data['end'] >= $period->start && $data['end'] <= $period->end) {
+                return redirect()->route('admin.period.create')->with('Error', 'There is already an ongoing period from ' . date('jS, F Y', strtotime($period->start)) . ' to ' . date('jS, F Y', strtotime($period->end)));
+            }
+            if ($data['start'] <= $period->start && $data['end'] >= $period->end) {
+                return redirect()->route('admin.period.create')->with('Error', 'There is already an ongoing period from ' . date('jS, F Y', strtotime($period->start)) . ' to ' . date('jS, F Y', strtotime($period->end)));
+            }
+        }
+
+        Period::create([
+            'term' => $data['term'],
+            'start' => $data['start'],
+            'end' => $data['end']
+        ]);
+        return redirect()->route('admin.period.index')->with('Success', 'Period Created!');
     }
 
     /**
@@ -58,7 +89,8 @@ class PeriodController extends Controller
      */
     public function edit(Period $period)
     {
-        //
+        $pages = 'period';
+        return view('admin.period.edit', compact('pages', 'period'));
     }
 
     /**
@@ -70,7 +102,37 @@ class PeriodController extends Controller
      */
     public function update(Request $request, Period $period)
     {
-        //
+        $data = $request->validate([
+            'term' => 'required',
+            'start' => 'required|date',
+            'end' => 'required|date',
+        ]);
+
+        if ($data['start'] >= $data['end']) {
+            return redirect()->route('admin.period.edit', $period->id)->with('Error', 'The starting date cannot exceed the end date');
+        }
+
+        $periods = Period::whereNotIn('id', [$period->id])->get();
+
+        foreach ($periods as $periodcheck) {
+            if ($data['start'] >= $periodcheck->start && $data['start'] <= $periodcheck->end) {
+                return redirect()->route('admin.period.edit', $period->id)->with('Error', 'There is already an ongoing period from ' . date('jS, F Y', strtotime($periodcheck->start)) . ' to ' . date('jS, F Y', strtotime($periodcheck->end)));
+            }
+            if ($data['end'] >= $periodcheck->start && $data['end'] <= $periodcheck->end) {
+                return redirect()->route('admin.period.edit', $period->id)->with('Error', 'There is already an ongoing period from ' . date('jS, F Y', strtotime($periodcheck->start)) . ' to ' . date('jS, F Y', strtotime($periodcheck->end)));
+            }
+            if ($data['start'] <= $periodcheck->start && $data['end'] >= $periodcheck->end) {
+                return redirect()->route('admin.period.edit', $period->id)->with('Error', 'There is already an ongoing period from ' . date('jS, F Y', strtotime($periodcheck->start)) . ' to ' . date('jS, F Y', strtotime($periodcheck->end)));
+            }
+        }
+
+        $period->update([
+            'term' => $data['term'],
+            'start' => $data['start'],
+            'end' => $data['end']
+        ]);
+
+        return redirect()->route('admin.period.index')->with('Success', 'Period Updated!');
     }
 
     /**
