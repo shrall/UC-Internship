e03c5fba7a9fdbbef1ce96bcd05c16ae2ec711c7
shrall <shrallvierdo@gmail.com>
2021-01-04 19:34:15 +0700

    api - project, offer, user, task - index - done

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
 use App\Http\Controllers\Api\Auth\LoginController;
-use App\Http\Controllers\Api\ProjectController;
+use App\Http\Controllers\Api\OfferController;
+use App\Http\Controllers\Api\Student\ProjectController as StudentProjectController;
+use App\Http\Controllers\Api\Student\UserController as StudentUserController;
+use App\Http\Controllers\Api\Supervisor\ProjectController as SupervisorProjectController;
+use App\Http\Controllers\Api\Supervisor\UserController as SupervisorUserController;
+use App\Http\Controllers\Api\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,6 +24,11 @@ Route::post('api-login', [LoginController::class, 'login']);
 Route::post('api-refresh', [LoginController::class, 'refresh']);
 
 Route::group(['middleware' => 'auth:api'], function () {
-    Route::apiResource('projects', ProjectController::class);
+    Route::apiResource('student/project', StudentProjectController::class);
+    Route::apiResource('supervisor/project', SupervisorProjectController::class);
+    Route::apiResource('student/user', StudentUserController::class);
+    Route::apiResource('supervisor/user', SupervisorUserController::class);
+    Route::apiResource('offer', OfferController::class);
+    Route::apiResource('task', TaskController::class);
     Route::post('api-logout', [LoginController::class, 'logout']);
 });
