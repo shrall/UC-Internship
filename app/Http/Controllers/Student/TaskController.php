a3df45e9edf4a2c1a96c08a1dc7799ee3ca48aaa
michellealexandra <62871142+michellealexandra@users.noreply.github.com>
2021-01-15 01:12:29 +0700

    lililili

--- app/Http/Controllers/Student/TaskController.php
+++ app/Http/Controllers/Student/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Student;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
--- resources/views/student/task/table/progress.blade.php
+++ resources/views/student/task/table/progress.blade.php
@@ -1,9 +1,11 @@
 @include('student.task.modal.progress.create')
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-4">
     <div>
+        @if($task->projectuser->project->status == 1)
         <a class="btn btn-secondary" data-toggle="modal" data-target="#modal-progress-create">
             <span class="fas fa-plus mr-2"></span>Add Progress
         </a>
+        @endif
     </div>
 </div>
 
